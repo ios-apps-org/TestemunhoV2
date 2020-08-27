fab1f75d0007f72072f7269c14563a90d9b678b2
Jon DeMaagd <jdemaagd7@outlook.com>
2020-08-27 18:22:18 -0400

    initial commit

---
+++ TestemunhoV2/CategoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestemunhoV2
+//
+//  Created by Jon DeMaagd on 8/27/20.
+//  Copyright © 2020 JON DEMAAGD. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ markdown/TODO.md
@@ -0,0 +1,6 @@
+# TODO: Core Data and UI/Unit Tests
+
+## QuestMapV2: Locations (POI)
+
+## CorredorV2: User, Location
+
